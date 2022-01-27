6371994642510d35be3244e3323e05e7de9e80ac
Kallahan <kallahan@eshop-guide.de>
2022-01-27 20:39:52 +0100

    table choosing implemented v.1

--- assets/reservation.js
+++ assets/reservation.js
@@ -2,12 +2,30 @@
 (function() {
 
     const tables = document.querySelectorAll(".table:not(.table--reserved)");
-    //console.log(tables);
+    const modal = document.querySelector('#reservationNotice');
+
     for (const table of tables) {
         table.addEventListener("click", showNotice);
     }
 
     function showNotice(event) {
-        console.log("index = " + event.target.dataset.tableNumber);
+        let tableNr = event.target.dataset.tableNumber;
+        let noticeBox = document.querySelector('#noticeContent');
+        noticeBox.innerHTML = "Wollen Sie den Tisch Nr. " + tableNr + " auswählen?";
+        modal.style.display = 'block';
     }
+
+    const closeButtons = document.querySelectorAll('span.close, .confirmation--close');
+    for (const btn of closeButtons) {
+        btn.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+      
 })();
